62c260ca911fef61e8ce0edf06e40bdb5e97f308
Shimul Mahmud <shimulmahmud18@gmail.com>
2022-05-25 22:02:45 +0600

    doctor's chamber frontend done

---
+++ src/components/Home/Doctors/Doctors.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import DoctorsDetail from "../DoctorsDetail/DoctorsDetail";
+const Doctors = () => {
+  const [doctors, setDoctors] = useState([]);
+  useEffect(() => {
+    fetch("https://rocky-forest-80852.herokuapp.com/doctors")
+      .then((res) => res.json())
+      .then((data) => setDoctors(data));
+  }, []);
+  return (
+    <section className="doctors">
+      <div className="container">
+        <h5 className="text-center  text-primary mb-5">Our Doctors</h5>
+        <div className="row">
+          {doctors.map((doctor) => (
+            <DoctorsDetail key={doctor._id} doctor={doctor} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Doctors;
---
+++ src/components/Home/Navbar/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a
+                className="nav-link ms-5 active"
+                aria-current="page"
+                href="/home"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ms-5" href="/dashboard/appointment">
+                Patient
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ms-5" href="/dashboard">
+                Dashboard
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ms-5 text-white" href="/dashboard">
+                Admin
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ms-5 text-white" href="!#">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ms-5 text-white" href="!#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
